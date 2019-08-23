c10fa58333f314b027fb0ef35d8c8db40b1f2487
David Cabrero <david.cabrero@madsgroup.org>
2019-08-23 16:35:53 +0200

    Versión preliminar

---
+++ assignment.md
@@ -0,0 +1,34 @@
+# Hello World _Assignment_
+
+Tu misión consiste en desarrollar el mítico *Hello World!*, seguido de
+algunas variaciones.
+
+## Check list
+
+Antes de comenzar, asegurate de que cuentas con las siguientes
+herramientas:
+
+* El lenguaje de programación `python` (versión >= 3.7).
+* Un repositorio *helloworld* en el servidor _gitlab_ de la FIC.
+
+  El repositorio tiene que estar configurado como *remote* del
+  repositorio local que utilizas para llevar a cabo la misión.
+
+
+# Task 1. Hello World!
+
+Crea un ejecutable con el nombre `helloworld.py` que imprima el
+mensaje "Hello World!".
+
+Antes de moverte a tu siguiente cometido (_task_), debes asegurarte de
+haber alcanzado las siguientes condiciones (_requirements_):
+
+* [Existe un fichero llamado "helloworld.py"](check:)
+* [El fichero "helloworld.py" es ejecutable](check:)
+* [Al ejecutar el fichero "helloworld.py" imprime el texto: "Hello
+  World!"](check:)
+* [No hay ficheros pendientes de commit](check:)
+* [El último commit tiene la etiqueta task-01](check:)
+* [El nombre del repositorio remote es helloworld](check:)
+* [El repositorio remote esta actualizado](check:)
+
---
+++ checks.py
@@ -0,0 +1,72 @@
+# coding: utf-8
+
+import sys
+import ast
+import re
+
+def pattern2regexp(pattern):
+    tree = ast.parse(f"f'{pattern}'")
+    values = tree.body[0].value.values
+    regexp = ""
+    for value in values:
+        if isinstance(value, ast.Str):
+            regexp += value.s
+        elif isinstance(value, ast.FormattedValue) and isinstance(value.value, ast.Name):
+            regexp += f"(?P<{value.value.id}>.+)"
+        else:
+            raise SyntaxError(pattern)
+    return re.compile(regexp)
+
+
+def get_check(text, module):
+    for regexp, builder in module.__checks__:
+        match = regexp.fullmatch(text)
+        if match is not None:
+            return builder(match)
+    return None
+
+    
+def requirement(pattern):
+    regexp = pattern2regexp(pattern)
+    def decorator(f):
+        def check_builder(match):
+            return lambda world: f(world, **match.groupdict())
+
+        module = sys.modules[f.__module__]
+        if not hasattr(module, "__checks__"):
+            module.__checks__ = []
+        module.__checks__.append((regexp, check_builder))
+        return f
+    return decorator
+
+
+# ----------------------------------------------------------------------
+
+@requirement("Existe un fichero llamado {filename}")
+def existe(world, filename):
+    assert False
+
+@requirement("El fichero {filename} es ejecutable")
+def es_ejecutable(world, filename):
+    assert False
+
+@requirement('Al ejecutar el fichero helloworld.py imprime el texto: "Hello World!"')
+def imprime_hello_world(world):
+    assert False
+
+@requirement("No hay ficheros pendientes de commit")
+def nothing_to_commit(world):
+    assert False
+
+@requirement("El último commit tiene la etiqueta {tagname}")
+def tiene_etiqueta(world, tagname):
+    assert False
+
+@requirement("El nombre del repositorio remote es {remote_name}")
+def el_remote_es(world, remote_name):
+    assert False
+
+@requirement("El repositorio remote esta actualizado")
+def el_remote_esta_up_to_date(world):
+    assert False
+
